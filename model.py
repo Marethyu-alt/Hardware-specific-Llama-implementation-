aad51210287e8af77ddfeea73898c23e49dc73e8
Ekkom Walia <ekkomjeev@gmail.com>
2026-08-29 02:22:27 +0000

    Transformer Skeleton Complete

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class LlamaConfig:
     n_layers:int = 32
     n_heads:int = 32
     n_kv_heads: Optional[int] = None # number of heads for the Keys and Queries
-    vocab_size: int = -1 #will change after loading tokenizer, likely 50k + 
+    vocab_size: int = -1 #will change after loading tokenizer, 30k
     multiple_of:int = 256 #multiplication constant
     ffn_dim_multiplier: Optional[float] = None #likely going to be 4 during MLP
     norm_eps:float = 1e-5 #random small epsilon norming
@@ -32,7 +32,20 @@ class Transformer(nn.Module):
             self.layers.append(EncoderBlock(config)) #pass into Block to be created later for layering transformer
         self.norm = RMSNorm(config.n_embd, eps = config.norm_eps)
         self.output = nn.Linear(config.n_embd, config.vocab_size, bias = False)
-        self.freqs_complex = precompute_theta_pos_frequencies(self.config.n_embd // self.config.n_heads, self.config.max_seq_len * 2, device = self.config.device)
+        self.freqs_complex = precompute_theta_pos_frequencies(self.config.n_embd // self.config.n_heads, self.config.max_seq_len * 2, device = self.config.device) #rope init
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, 'KV cache requires seq len = 1' 
+        h = self.tok_embeddings(tokens) #(B,T) * (C, n_embd) = (B,T,n_emmbd) dictionary lookup (32,2048,4096)
+        freqs_complex = self.freqs_complex[start_pos: star_pos + seq_len] #ensuring dynamic accumulation of tokens also kv cache
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_complex) #consecutively apply the encoder to all layers
+        h = self.norm(h)
+        output = self.output(h).float()
+        return output #transformer skeleton complete 
+    
+
+
 
 
 if __name__ == '__main__': 
