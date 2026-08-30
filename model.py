bde6d5f8b0b772a0b52667ebb1db9bf326bbf3b7
Ekkom Walia <ekkomjeev@gmail.com>
2026-08-30 04:05:44 +0000

    Cleaning up

--- model.py
+++ model.py
@@ -21,6 +21,30 @@ class LlamaConfig:
     max_seq_len: int = 2048  #T = 2048
     device:str = None #move to CPU by default later, weights pre-loaded 
 
+
+#Understanding ROPE: 
+#Absolute Positional Encodings = xiWQ * xjWK.T / sqrt d z 
+#Relative Positional Encodings (NOT USED IN LLAMA) = xiWQ(xjWk + aKij)T / sqrt dz  = xiWQ* xjWk.T/sqrt dz (content to content similarity) + xiWq*aKij.T / sqrt dz (content to relative position term)
+#Fundamental Question: can we find an inner product or similarity over vectors of q and k such that the attention mechanism depends only on the two vectors and their distance?
+#Rotary Position Embeddings: Attention Score = (q1k1 + q2k2)cos(m-n)theta + (q1k2-q2k1)sin(m-n)theta for a 2-d vector this results in a number that represents both meaning(semantic positions which is found through backprop and multi-head attention) and distance (position)
+#Simplified Formula using complex numbers for computational efficiency: Attention Score = (q*k)*[e^i(m-n)theta] (Euler's formula)
+
+#ROPE implementation
+def precompute_theta_pos_frequencies(head_dim, seq_len, device, theta: float = 10_000.0):
+    assert head_dim % 2 == 0, 'Head dims must be even for ROPE'
+    theta_numerator = torch.arange(0,head_dim,2).float() #formula theta = 10k ^  (-2(i-1) / head_dim) for i = 1,2,3..head_dim/2 = 1/10,000 ^ 2((i-1)/head_dim), setting up different speeds of theta
+    theta = 1 / (theta ** (theta_numerator/ head_dim)).to(device) #(head_dim / 2) = shape
+    m = torch.arange(seq_len, device = device)
+    freqs = torch.outer(m, theta).float() #(seq_len, headdim/2), each individual element of m times all values of theta, all positions of tokens and their corresponding angles/frequencies
+    freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_complex #matrix of (seq_len, head_dim/2) literally just positions of tokens and by how much each is rotated respectively
+def apply_rotary_embeddings(x,freqs_complex, device):
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1],-1,2)) #(B, seq_len, H, head_dim) -> (B,seq_len,, head_dim/2 (real_dim, imag_dim)) by dividing by 2 then converting to imaginary, token features (x)
+    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2) #adding 2 extra dims for B, H (1, Seq_len, 1, head_dim/2)
+    x_rotated = x_complex * freqs_complex #multiplies actual tokens by their corresponding frequencies
+    x_out = torch.view_as_real(x_rotated) #returning back to real numbers to be computed via gpu with ease
+    x_out = x_out.reshape(*x.shape)
+    return x_out.type_as(x).to(device)
 #main transformer
 class Transformer(nn.Module): 
     def __init__(self,config): #takes as arg config: LlamaConfig will be passed into config later 
@@ -44,36 +68,6 @@ class Transformer(nn.Module):
         h = self.norm(h)
         output = self.output(h).float() #RMS Norm (32,2048,4096) * (4096,30,000) = (32,2048,30,000) or (B,T,C)
         return output #transformer skeleton complete 
-    
-#Understanding ROPE: 
-#Absolute Positional Encodings = xiWQ * xjWK.T / sqrt d z 
-#Relative Positional Encodings (NOT USED IN LLAMA) = xiWQ(xjWk + aKij)T / sqrt dz  = xiWQ* xjWk.T/sqrt dz (content to content similarity) + xiWq*aKij.T / sqrt dz (content to relative position term)
-#Fundamental Question: can we find an inner product or similarity over vectors of q and k such that the attention mechanism depends only on the two vectors and their distance?
-#Rotary Position Embeddings: Attention Score = (q1k1 + q2k2)cos(m-n)theta + (q1k2-q2k1)sin(m-n)theta for a 2-d vector this results in a number that represents both meaning(semantic positions which is found through backprop and multi-head attention) and distance (position)
-#Simplified Formula using complex numbers for computational efficiency: Attention Score = (q*k)*[e^i(m-n)theta] (Euler's formula)
-
-
-#rope implementation
-    def precompute_theta_pos_frequencies(head_dim, seq_len, device, theta: float = 10_000.0):
-        assert head_dim % 2 == 0, 'Head dims must be even for ROPE'
-        theta_numerator = torch.arange(0,head_dim,2).float() #formula theta = 10k ^  (-2(i-1) / head_dim) for i = 1,2,3..head_dim/2 = 1/10,000 ^ 2((i-1)/head_dim), setting up different speeds of theta
-        theta = 1 / (theta ** (theta_numerator/ head_dim)).to(device) #(head_dim / 2) = shape
-        m = torch.arange(seq_len, device = device)
-        freqs = torch.outer(m, theta).float() #(seq_len, headdim/2), each individual element of m times all values of theta, all positions of tokens and their corresponding angles/frequencies
-        freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
-        return freqs_complex #matrix of (seq_len, head_dim/2) literally just positions of tokens and by how much each is rotated respectively
-    def apply_rotary_embeddings(x,freqs_complex, device):
-        x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1],-1,2)) #(B, seq_len, H, head_dim) -> (B,seq_len,, head_dim/2 (real_dim, imag_dim)) by dividing by 2 then converting to imaginary, token features (x)
-        freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2) #adding 2 extra dims for B, H (1, Seq_len, 1, head_dim/2)
-        x_rotated = x_complex * freqs_complex #multiplies actual tokens by their corresponding frequencies
-        x_out = torch.view_as_real(x_rotated) #returning back to real numbers to be computed via gpu with ease
-        x_out = x_out.reshape(*x.shape)
-        return x_out.type_as(x).to(device)
-
-
-        
-
-
     
 
 
